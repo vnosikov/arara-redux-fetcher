4e6b59e914b528507b8a6a612812f69422b58c25
vnosikov <vnosikov@gmail.com>
2019-10-16 13:26:35 -0300

    replacing simple fetcher with a special case of a complex fetcher

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import withAraraFetcher from './simple';
+// import withAraraFetcher from './simple';
 import withComplexAraraFetcher from './complex';
 import fetcherReducer from './fetcherReducer';
+
+
+const withAraraFetcher = (
+  Component,
+  path,
+  reduxDataKey,
+  fetchParamNames = [],
+  reduxRoot = 'pages',
+) => withComplexAraraFetcher(
+  Component,
+  [path],
+  [reduxDataKey],
+  ['data'],
+  [fetchParamNames],
+  reduxRoot,
+);
 
 export {
   withAraraFetcher,
